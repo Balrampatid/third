0a990da979019d23e6477be560de238e150fa495
Balram Patidar <balram421021@gmail.com>
2025-04-12 11:25:40 +0530

    Add files via upload

---
+++ Third.js
@@ -0,0 +1,22 @@
+fucntion add(a,b)
+{
+    var c=a+b;
+    console.log(c);
+}
+fucntion sub(a,b)
+{
+    var c=a-b;
+    console.log(c);
+}
+fucntion multi(a,b)
+{
+    var c=a+b;
+    console.log(c);
+}
+//multi(10,20)
+fucntion Test(a,b)
+{
+
+    add(a,b);
+}
+Test(10,20);
